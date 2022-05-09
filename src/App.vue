cf2409af5833e3a1827ea1a8d79edd6da554ddd3
Bang-Bo Zheng <zhengbangbo@hotmail.com>
2022-05-10 01:54:20 +0800

    feat: add footer component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
-  <Header></Header>
+  <div>
+    <Header />
+    <Footer />
+  </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Footer from './components/Footer'
 export default {
   components: {
     Header,
+    Footer
   },
 }
 </script>
